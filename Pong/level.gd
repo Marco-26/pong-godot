8806d3bc941319719c52e752a4d46947d538fd8c
Marco <markcostah@gmail.com>
2022-04-10 21:53:56 +0100

    score system

--- Pong/level.gd
+++ Pong/level.gd
@@ -8,6 +8,8 @@ signal change_score
 enum score_options {opponent, player}
 var score_goes_to
 
+var end_game = false
+
 onready var timer = $RoundDelay
 
 func _ready():
@@ -34,18 +36,29 @@ func _handle_ball_collisions(wall_collided):
 	_check_score()
 	emit_signal("change_score", score_goes_to)
 	yield(get_tree().create_timer(2), "timeout")
-	get_tree().reload_current_scene()
+	
+	if end_game == false:
+		get_tree().reload_current_scene()
+	else:
+		_restart_game()
 
 func _check_score():
 	var winner
 	# fazer sinal com o parametro de quem ganha o jogo para o hud atualizar a label winner
-	var finish = 1
+	var finish = 2
 	if(Globals.enemy_score >= finish):
 		winner = "Enemy"
+		end_game = true
 	elif(Globals.player_score >= finish):
 		winner = "Player"
+		end_game = true
 	
 	emit_signal("winner_signal", winner)
+
+func _restart_game():
+	Globals.enemy_score = 0
+	Globals.player_score = 0
+	get_tree().reload_current_scene()
 
 func _on_RoundDelay_timeout():
 	get_tree().paused = false
